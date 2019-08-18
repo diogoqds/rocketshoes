8bef4dce05664c3b4fee41aaf8d48b80eac85749
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-08-18 13:54:18 -0300

    request products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      {[1, 2, 3, 4, 5, 6].map(i => (
-        <li key={i}>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?resize=326:*"
-            alt="tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,90</span>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={20} color="#fff" />
-            </div>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const products = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products });
+  }
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
-    </ProductList>
-  );
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={20} color="#fff" />
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
